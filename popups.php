5c0a9b4c5e2347cd8ba2119a90beb20eed22f155
fetitxe <fetitxe@users.noreply.github.com>
2019-04-10 12:11:39 +0200

    Update popups.php

--- popups.php
+++ popups.php
@@ -5,13 +5,13 @@
  * @package   socialpopup
  * @author    Damian Logghe <info@timersys.com>
  * @license   GPL-2.0+
- * @link      https://timersys.com/free-plugins/popups/
+ * @link      http://www.timersys.com/plugins-wordpress/social-popup/
  * @copyright 2014 Damian Logghe
  *
  * @socialpopup
  * Plugin Name:       Popups - WordPress Popup
- * Plugin URI:        https://timersys.com/free-plugins/popups/
- * Version: 		  1.9.3.5
+ * Plugin URI:        http://www.timersys.com/free-plugins/social-popup/
+ * Version: 		  1.9.3.6
  * Description: 	  Most complete free Popups plugin, scroll triggered popups, compatible with social networks, Gravity Forms, Ninja Forms, Contact form 7, Mailpoet, Mailchimp for WP, Postmatic, etc
  * Author: 			  timersys
  * Author URI:        https://timersys.com
@@ -30,7 +30,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Public-Facing Functionality
  *----------------------------------------------------------------------------*/
 
-define( 'SPU_VERSION' , '1.9.3.5' );
+define( 'SPU_VERSION' , '1.9.3.6' );
 define( 'SPU_PLUGIN_DIR' , plugin_dir_path(__FILE__) );
 define( 'SPU_PLUGIN_URL' , plugin_dir_url(__FILE__) );
 define( 'SPU_PLUGIN_HOOK' , basename( dirname( __FILE__ ) ) . '/' . basename( __FILE__ ) );
